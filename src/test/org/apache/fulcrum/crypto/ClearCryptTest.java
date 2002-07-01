edee3687d07f0e4e024983f12666587060f6d280
Henning Schmiedehausen <henning@apache.org>
2002-07-01 16:33:38 +0000

    This is the Crypto Service implementation as proposed for Turbine 2.2 some while ago. I ported it to Fulcrum and we're using it for about six months without any problems.
    Documentation and Tests included.
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/fulcrum/trunk@222509 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/fulcrum/crypto/ClearCryptTest.java
@@ -0,0 +1,151 @@
+package org.apache.fulcrum.crypto;
+
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and 
+ *    "Apache Turbine" must not be used to endorse or promote products 
+ *    derived from this software without prior written permission. For 
+ *    written permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Apache Turbine", nor may "Apache" appear in their name, without 
+ *    prior written permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+import junit.framework.TestCase;
+
+import org.apache.fulcrum.ServiceManager;
+import org.apache.fulcrum.TurbineServices;
+
+import org.apache.fulcrum.factory.FactoryService; 
+import org.apache.fulcrum.factory.TurbineFactoryService; 
+
+import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+
+/**
+ * Testcase for the CryptoService.
+ *
+ * Objective: Checks, whether the supplied "clear" crypto
+ * provider can be selected and offers "clear" (which is
+ * unencrypted) Encryption.
+ *
+ * @author <a href="mailto:hps@intermeta.de">Henning P. Schmiedehausen</a>
+ * @version $Id$
+ *
+ */
+
+public class ClearCryptTest
+    extends TestCase
+{
+    private static final String PREFIX = "services." +
+        CryptoService.SERVICE_NAME + '.';
+
+    private String input   = "Oeltanks";
+    private String result  = "Oeltanks";
+
+    public ClearCryptTest( String name )
+    {
+        super(name);
+    }
+
+
+    public void setUp()
+    {
+    }
+
+    public void tearDown()
+    {
+    }
+
+    public void testConstruction()
+    {
+    }
+
+    public void testSelection()
+    {
+        try
+        {
+            doit();
+        }
+        catch( Exception e )
+        {
+            fail( e.getMessage() );
+        }
+    }
+
+    public void doit()
+        throws Exception
+    {
+        ServiceManager serviceManager = TurbineServices.getInstance();
+        serviceManager.setApplicationRoot(".");
+        
+        Configuration cfg = new BaseConfiguration();
+        cfg.setProperty(PREFIX + "classname",
+                        TurbineCryptoService.class.getName());
+        cfg.setProperty(PREFIX + "algorithm.unix",
+                        "org.apache.fulcrum.crypto.provider.UnixCrypt");
+        cfg.setProperty(PREFIX + "algorithm.clear",
+                        "org.apache.fulcrum.crypto.provider.ClearCrypt");
+
+        /* Ugh */
+
+        cfg.setProperty("services." + FactoryService.SERVICE_NAME + ".classname",
+                        TurbineFactoryService.class.getName());
+
+        serviceManager.setConfiguration(cfg);
+
+        serviceManager.init();
+
+        CryptoAlgorithm ca = TurbineCrypto.getService().getCryptoAlgorithm("clear");
+
+        String output = ca.encrypt(input);
+
+        if(!output.equals(result))
+        {
+            fail("Encryption failed, expected "+result+", got "+output);
+        }
+    }
+}
