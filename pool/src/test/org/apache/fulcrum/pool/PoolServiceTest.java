94b0fb96a5158cccf71496a453a5a204fa196efc
David Eric Pugh <epugh@apache.org>
2003-08-20 00:47:19 +0000

    Adding redone pool component.  Has Unit Tests!
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/fulcrum/trunk@222684 13f79535-47bb-0310-9956-ffa450edef68

---
+++ pool/src/test/org/apache/fulcrum/pool/PoolServiceTest.java
@@ -0,0 +1,135 @@
+/*
+ * Created on Aug 20, 2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.fulcrum.pool;
+import org.apache.avalon.framework.component.ComponentException;
+import org.apache.fulcrum.testcontainer.BaseUnitTest;
+/**
+ * @author Eric Pugh
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class PoolServiceTest extends BaseUnitTest
+{
+    private PoolService poolService = null;
+    /**
+    	* Defines the testcase name for JUnit.
+    	*
+    	* @param name the testcase's name.
+    	*/
+    public PoolServiceTest(String name)
+    {
+        super(name);
+    }
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(PoolServiceTest.class);
+    }
+    protected void setUp()
+    {
+        super.setUp();
+        try
+        {
+            poolService = (PoolService) this.lookup(PoolService.ROLE);
+        }
+        catch (ComponentException e)
+        {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+    /*
+     * Class to test for Object getInstance(String)
+     * @todo Some sort of compile error prevents this..
+     */
+    public void testGetInstanceString() throws PoolException
+    {
+        //Object object = poolService.getInstance("java.lang.StringBuffer");
+        //assertTrue(object instanceof StringBuffer);
+    }
+    /*
+     * Class to test for Object getInstance(String, ClassLoader)
+     */
+    public void testGetInstanceStringClassLoader()
+    {
+    }
+    /*
+     * Class to test for Object getInstance(String, Object[], String[])
+     */
+    public void testGetInstanceStringObjectArrayStringArray()
+    {
+    }
+    /*
+     * Class to test for Object getInstance(String, ClassLoader, Object[], String[])
+     */
+    public void testGetInstanceStringClassLoaderObjectArrayStringArray()
+    {
+    }
+    public void testIsLoaderSupported()
+    {
+    }
+    /*
+     * Class to test for Object getInstance(Class)
+     */
+    public void testGetInstanceClass() throws PoolException
+    {
+        Object object = poolService.getInstance(StringBuffer.class);
+        assertTrue(object instanceof StringBuffer);
+    }
+    /*
+     * Class to test for Object getInstance(Class, Object[], String[])
+     */
+    public void testGetInstanceClassObjectArrayStringArray()
+    {
+    }
+    public void testPutInstance()
+    {
+        String s = "I am a string";
+        assertEquals(0, poolService.getSize("java.lang.String"));
+        poolService.putInstance(s);
+        assertEquals(1, poolService.getSize("java.lang.String"));
+    }
+    public void testGetSetCapacity()
+    {
+        assertEquals(128, poolService.getCapacity("java.lang.String"));
+        poolService.setCapacity("java.lang.String", 278);
+        assertEquals(278, poolService.getCapacity("java.lang.String"));
+    }
+    public void testGetSize()
+    {
+        String s = "I am a string";
+        assertEquals(0, poolService.getSize("java.lang.String"));
+        poolService.putInstance(s);
+        assertEquals(1, poolService.getSize("java.lang.String"));
+    }
+    /*
+     * Class to test for void clearPool(String)
+     */
+    public void testClearPoolString()
+    {
+        String s = "I am a string";
+        assertEquals(0, poolService.getSize("java.lang.String"));
+        poolService.putInstance(s);
+        assertEquals(1, poolService.getSize("java.lang.String"));
+        poolService.clearPool("java.lang.String");
+        assertEquals(0, poolService.getSize("java.lang.String"));
+    }
+    /*
+     * Class to test for void clearPool()
+     */
+    public void testClearPool()
+    {
+        String s = "I am a string";
+        assertEquals(0, poolService.getSize("java.lang.String"));
+        poolService.putInstance(s);
+        poolService.putInstance(new Double(32));
+        assertEquals(1, poolService.getSize("java.lang.String"));
+        poolService.clearPool();
+        assertEquals(0, poolService.getSize("java.lang.String"));
+        assertEquals(0, poolService.getSize("java.lang.Double"));
+    }
+}
