b4fb9e563c17aee50129b41cde46d7d2a96d245c
David Eric Pugh <epugh@apache.org>
2003-09-14 13:30:16 +0000

    Commit a turbine based example of using the workflow component.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/fulcrum/trunk@222751 13f79535-47bb-0310-9956-ffa450edef68

---
+++ osworkflow/example/src/java/org/apache/fulcrum/workflow/example/modules/actions/Index.java
@@ -0,0 +1,33 @@
+package org.apache.fulcrum.workflow.example.modules.actions;
+
+import org.apache.turbine.modules.actions.VelocityAction;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+
+
+
+/**
+ * @author     <a href="mailto:epugh@upstate.com">Eric Pugh</a>
+ * @created    October 11, 2002
+ */
+public class Index extends VelocityAction {
+
+	/**
+	 *  Default action is to load up the default template and list out the locations to go to:
+	 <ol><li>Inventory Add Request<li>Inventory Delete Request<li>General Admin of Request</ol>
+	 *
+	 * @param  data           Current RunData information
+	 * @param  context        Context to populate
+	 * @exception  Exception  Thrown on error
+	 */
+	public void doPerform(RunData data, Context context) throws Exception {
+
+		System.out.println("Doperform called");
+		data.setScreenTemplate("Index.vm");
+
+	}
+
+	
+	
+
+}
---
+++ osworkflow/example/src/webapp/index.jsp
@@ -0,0 +1,13 @@
+
+
+
+
+
+<%@ taglib uri="/WEB-INF/response.tld" prefix="res" %>
+<res:sendRedirect>
+  <res:encodeRedirectUrl>/fulcrum-workflow-example/servlet/fulcrum-workflow-example/action/Index</res:encodeRedirectUrl>
+</res:sendRedirect>
+
+Redirecting to <A href="/fulcrum-workflow-example/servlet/fulcrum-workflow-example/action/Index">Fulcrum Workflow Example home page</a>.  	
+
+        		
