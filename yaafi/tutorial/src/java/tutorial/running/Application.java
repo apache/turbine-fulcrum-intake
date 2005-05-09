7e83652ab153b21020fee12cc0c2ac68a500a312
Siegfried Goeschl <sgoeschl@apache.org>
2005-05-09 17:52:50 +0000

    Added a simple YAAFI tutorial
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/turbine/fulcrum/trunk@223324 13f79535-47bb-0310-9956-ffa450edef68

---
+++ yaafi/tutorial/src/java/tutorial/running/Application.java
@@ -0,0 +1,106 @@
+package tutorial.running;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import org.apache.avalon.framework.logger.ConsoleLogger;
+import org.apache.fulcrum.yaafi.cli.Main;
+
+/**
+ * Test suite for exercising the command line integration.
+ *
+ * @author <a href="mailto:siegfried.goeschl@it20one.at">Siegfried Goeschl</a>
+ */
+
+public class Application implements Runnable
+{
+    /** the YAAFI command line interface */
+    private Main cli;
+
+    public static void main( String[] args )
+    {
+        try
+        {
+            new Application(args).init().run();
+        }
+        catch( Throwable t )
+        {
+            String msg = "Execution of the server failed : " + t.getMessage();
+            System.err.println(msg);
+        }
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param args the command line parameters
+     */
+    public Application(String[] args)
+    {
+        this.cli = new Main(args);
+    }
+
+    /**
+     * Initialize the application
+     * 
+     * @return the initialized instance
+     * @throws Exception the initialization failed
+     */
+    protected Application init() throws Exception
+    {
+        // 1) initialize the YAAFI Main class
+
+        // 1.1) set the temp directory to be used
+        this.cli.setTempHome( "./tutorial/temp" );
+
+        // 1.2) set the container configuration to bootstrap the YAAFI container
+        this.cli.setContainerConfigValue( "./tutorial/conf/containerConfiguration.xml" );
+
+        // 1.3) block the main thread until the JVM is terminated
+        this.cli.setIsBlocking(true);
+
+        // 1.4) install a JVM shutdown hook to dispose the YAAFI container
+        this.cli.setHasShutdownHook(true);
+
+        // 2) initialize the logger
+
+        ConsoleLogger consoleLogger = new ConsoleLogger(ConsoleLogger.LEVEL_DEBUG);
+        this.cli.setLogger( consoleLogger );
+
+        return this;
+    }
+    
+    /**)
+     * @see java.lang.Runnable#run()
+     */
+    public void run()
+    {
+		try
+        {
+            this.cli.initialize();
+            this.cli.getLogger().info( "The application is up and running ..." );			
+            this.cli.onWait();
+            this.cli.getLogger().info( "The application is terminating ..." );
+        }
+        catch (Throwable t)
+        {
+            String msg = "Running the server failed due to : " + t.getMessage();
+            this.cli.getLogger().error(msg,t);
+            throw new RuntimeException(msg);
+        }
+    }    
+ }
---
+++ yaafi/tutorial/src/java/tutorial/service/SystemPropertyServiceImpl.java
@@ -0,0 +1,89 @@
+package tutorial.service;
+
+/*
+ * Copyright 2004 Apache Software Foundation
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.avalon.framework.activity.Initializable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.configuration.Reconfigurable;
+import org.apache.avalon.framework.context.Context;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.context.Contextualizable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+
+
+/**
+ * A slightly more complex tuturial example.
+ *
+ * @author <a href="mailto:siegfried.goeschl@it20one.at">Siegfried Goeschl</a>
+ */
+
+public class SystemPropertyServiceImpl
+    extends AbstractLogEnabled
+    implements SystemPropertyService, Reconfigurable, Contextualizable, Initializable
+{
+    /** the Avalon temp directory */
+    private File tempDir;
+        
+    /**
+     * @see org.apache.avalon.framework.context.Contextualizable#contextualize(org.apache.avalon.framework.context.Context)
+     */
+    public void contextualize(Context context) throws ContextException
+    {        
+        this.tempDir = (File) context.get("urn:avalon:temp");
+    }
+    
+    /**
+     * @see org.apache.avalon.framework.configuration.Configurable#configure(org.apache.avalon.framework.configuration.Configuration)
+     */
+    public void configure(Configuration configuration) throws ConfigurationException
+    {
+        Configuration[] systemProperties = configuration.getChildren("property");
+
+        for( int i=0; i<systemProperties.length; i++ )
+        {
+            String key = systemProperties[i].getAttribute("name");
+            String value = systemProperties[i].getValue();
+            this.getLogger().debug( "Setting the value of " + key + " to " + value );
+            System.setProperty( key, value );
+        }        
+    }
+
+    /**
+     * @see org.apache.avalon.framework.activity.Initializable#initialize()
+     */
+    public void initialize() throws Exception
+    {
+        FileOutputStream fos = new FileOutputStream( new File(this.tempDir,"system.properties") );
+        System.getProperties().store( fos, "system.properties" );
+        fos.flush();
+        fos.close();
+    }
+    
+    /**
+     * @see org.apache.avalon.framework.configuration.Reconfigurable#reconfigure(org.apache.avalon.framework.configuration.Configuration)
+     */
+    public void reconfigure(Configuration configuration)
+        throws ConfigurationException
+    {
+        this.configure(configuration);
+    }
+}
