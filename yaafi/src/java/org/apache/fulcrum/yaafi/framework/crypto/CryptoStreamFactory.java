45eb56ca1f5b72fb210e1a30b020e2fe4cac680d
Siegfried Goeschl <sgoeschl@apache.org>
2008-09-29 21:33:23 +0000

    Making transparent decryption of configuration files really optional - this makes me and Gump happy
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk@700265 13f79535-47bb-0310-9956-ffa450edef68

--- yaafi/src/java/org/apache/fulcrum/yaafi/framework/crypto/CryptoStreamFactory.java
+++ yaafi/src/java/org/apache/fulcrum/yaafi/framework/crypto/CryptoStreamFactory.java
@@ -25,7 +25,9 @@ import java.io.InputStream;
 
 /**
  * Factory class to get a decrypting input stream for reading configuration
- * files. 
+ * files. The implementation uses dynamic class loading to make decryption
+ * an optional feature which is highly desirable when avoiding the ECCN
+ * export code problems.
  *
  * @author <a href="mailto:siegfried.goeschl@it20one.at">Siegfried Goeschl </a>
  */
@@ -42,11 +44,12 @@ public class CryptoStreamFactory
     private static String className = "org.apache.fulcrum.jce.crypto.CryptoStreamFactoryImpl";
 
     /**
-     * Create a decrypting input stream using the default password.
+     * Create a (potentially) decrypting input stream using the default
+     * password.
      *
      * @param is the input stream to be decrypted
      * @param isEncrypted the encryption mode (true|false|auto)
-     * @return an decrypting input stream
+     * @return a decrypting input stream
      * @throws Exception reading the input stream failed
      */
     public static InputStream getDecryptingInputStream( InputStream is, String isEncrypted )
@@ -56,14 +59,22 @@ public class CryptoStreamFactory
         
         if( isEncrypted.equalsIgnoreCase("true") )
         {
+            // a decrypting input stream was requested
             result = createDecryptingInputStream(is, "getInputStream");
         }
-        else if( isEncrypted.equalsIgnoreCase("auto") )
+        else if( isEncrypted.equalsIgnoreCase("auto") && hasCryptoStreamFactory())
         {
+            // no user-supplied preferences but crypto stream is available
             result = createDecryptingInputStream(is, "getSmartInputStream");
+        }
+        else if( isEncrypted.equalsIgnoreCase("auto") && !hasCryptoStreamFactory())
+        {
+            // no user-supplied perferences so we fall back to normal input stream
+            result = is;
         }
         else if( isEncrypted.equalsIgnoreCase("false") )
         {
+            // just use normal input stream
             result = is;
         }
         else
@@ -76,8 +87,13 @@ public class CryptoStreamFactory
 
     /**
      * Factory method to create a decrypting input stream.
+     *
+     * @param is the input stream to be decrypted
+     * @param factoryMethodName the name of the factory method
+     * @return a decrypting input stream
+     * @throws Exception creating the decrypting input stream failed
      */
-    private static InputStream createDecryptingInputStream( InputStream is, String methodName )
+    private static InputStream createDecryptingInputStream( InputStream is, String factoryMethodName )
         throws Exception
     {
         Class[] signature = {InputStream.class};
@@ -90,7 +106,7 @@ public class CryptoStreamFactory
         }
         else
         {
-            return (InputStream) Clazz.invoke(cryptoStreamFactory, methodName, signature, args);
+            return (InputStream) Clazz.invoke(cryptoStreamFactory, factoryMethodName, signature, args);
         }
     }
 
@@ -115,5 +131,14 @@ public class CryptoStreamFactory
         }
                     
         return cryptoStreamFactory;
+    }
+
+    /**
+     * @return true if a CryptoStreamFactory is available
+     */
+    private static boolean hasCryptoStreamFactory()
+        throws Exception
+    {
+        return ( getCryptoStreamFactory() != null );
     }
 }
