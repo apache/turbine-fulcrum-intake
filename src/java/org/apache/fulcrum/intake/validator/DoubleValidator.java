52ae93da45b2c5464aded1df6d24fe84a14432b7
Thomas Vandahl <tv@apache.org>
2018-02-11 17:11:55 +0000

    TRB-94: Align validation of numbers with ValueParser behavior
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk/intake@1823857 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/fulcrum/intake/validator/DoubleValidator.java
+++ src/java/org/apache/fulcrum/intake/validator/DoubleValidator.java
@@ -1,26 +1,5 @@
 package org.apache.fulcrum.intake.validator;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.Locale;
 
 /**
@@ -62,15 +41,7 @@ public class DoubleValidator
     @Override
     protected Double parseNumber(String stringValue, Locale locale) throws NumberFormatException
     {
-        NumberFormat nf = NumberFormat.getInstance(locale);
-
-        try
-        {
-			return Double.valueOf(nf.parse(stringValue).doubleValue());
-		}
-        catch (ParseException e)
-        {
-        	throw new NumberFormatException(e.getMessage());
-		}
+        Number number = parseIntoNumber(stringValue, locale);
+		return Double.valueOf(number.doubleValue());
     }
 }
