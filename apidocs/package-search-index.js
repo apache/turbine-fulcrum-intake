4a00bc0803333ded3a7de77eee4c1c806fa8928e
Georg Kallidis <gk@apache.org>
2021-09-14 08:39:30 +0100

    Initial generated maven site content and .asf.yaml file with subdir set

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.apache.fulcrum.intake"},{"l":"org.apache.fulcrum.intake.model"},{"l":"org.apache.fulcrum.intake.validator"}]
